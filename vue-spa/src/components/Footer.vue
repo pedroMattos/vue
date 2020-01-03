fbcb53c55e54a33896154fd43051412b097ac78b
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-03 02:59:23 -0300

    add footer template

---
+++ vue-spa/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <div class="row">
+            <div class="col md3">
+
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import bd from './firebaseinit'
+export default {
+    name: 'footer',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
