82374125428aaee38e13988761d6a0701980fe37
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-03 12:28:11 -0300

    add films component

---
+++ vue-spa/src/components/Films.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="main-films">
+
+    </div>
+</template>
+
+<script>
+import cards from './Cards'
+import banner from './banner-internal'
+export default {
+    name: 'games-page',
+    components: {
+        cards,
+        banner,
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
