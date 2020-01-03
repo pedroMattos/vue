e43360c0d3d665bd5bc191b8879e25975f36afe9
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-03 12:28:56 -0300

    add indexFilmes

---
+++ vue-spa/src/components/indexFilms.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main">
+        <banner/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import banner from './banner-internal'
+import single from './singleContent'
+export default {
+    name: 'indexFilms',
+    components: {
+        banner,
+        single,
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
