2ae938151a28ec326a2dbf077047bcc3abed198d
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-04 00:56:59 -0300

    add auth method

--- vue-spa/src/components/firebaseinit.js
+++ vue-spa/src/components/firebaseinit.js
@@ -3,12 +3,5 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
-export default firebaseApp.firestore();
-
-// export default function install (Vue) {
-//     Object.defineProperty(Vue.prototype, '$firebase', {
-//         get() {
-//             return firebaseApp
-//         }
-//     })
-// }
+// exporta o metodo de autenticação e o banco de dados
+export default firebaseApp.auth() && firebaseApp.firestore();
